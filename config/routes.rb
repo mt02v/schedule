33e6d7554bcf1e2323ca713296fdba5f2d451320
mt02v <141305346+mt02v@users.noreply.github.com>
2023-12-16 15:55:06 +0000

    [Finish] schedule

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
+  #get 'tasks/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+resources :tasks # ルーティングをまとめて設定
+root to: 'tasks#index' # トップページの設定
 end
